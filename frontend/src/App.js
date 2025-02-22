a91b96f66fb32c9409ce1bdf41ce6183d6ac5844
Catalin Danis <geaigelsaflu@gmail.com>
2025-02-22 17:21:42 +0200

    Finished add transaction functionality between backend and frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import {useState} from "react";
+import axios from "axios";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     const [type, setType] = useState(null);
     const [firstRadio, setFirstRadio] = useState(null);
     const [operationMessage, setOperationMessage] = useState(null);
+    //const axios = require('axios');
 
     return (
         <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
@@ -15,7 +17,8 @@ function App() {
                 <img src="./logo.png" style={{height: 25, marginRight: 10}}/>
                 <h1>Bank account</h1>
             </div>
-                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+            <div style={{border: '1px solid black', borderRadius: 10, paddingLeft: 10, paddingRight: 10, paddingBottom: operationMessage == null ? 5 : 0}}>
+                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center', marginTop: -10}}>
                     <div style={{marginRight: 20}}>
                         <h4 style={{marginBottom: 0}}>Amount</h4>
                         <input type={"number"} onChange={e => setAmount(e.target.value)} placeholder="Enter amount:"/>
@@ -39,28 +42,49 @@ function App() {
                     </div>
                     <div>
                         <button style={{marginTop: 45}} onClick={() => {
-                            if(amount == null || amount <= 0) {
+                            if (amount == null || amount <= 0) {
                                 setOperationMessage("Please enter amount");
                                 return;
                             }
-                            if(isNaN(new Date(date))) {
+                            if (isNaN(new Date(date)) || date == null) {
                                 setOperationMessage("Please enter a valid date");
                                 return;
                             }
-                            if(type == null) {
+                            if (type == null) {
                                 setOperationMessage("Please select type");
                                 return;
                             }
 
-                            setOperationMessage("Transaction added successfully");
+                            const result = addTransaction(amount, date, type);
+                            setOperationMessage(result);
                         }}>
                             Add transaction
                         </button>
                     </div>
                 </div>
-                <p>{operationMessage}</p>
+                <p style={{marginTop: 5, marginBottom: 5}}>{operationMessage}</p>
             </div>
+        </div>
     );
+}
+
+async function addTransaction(amount, date, type){
+    try{
+        const response = await axios.post('http://localhost:8080/add', null,
+            {
+                params: {
+                    date: date.toString(),
+                    amount: amount,
+                    type: type.toString(),
+                }
+            })
+        if(response.status == 200)
+            return "Transaction added successfully"
+        return response.data;
+    }
+    catch (error) {
+        return "An unknown error occurred. Check the log to see more details."
+    }
 }
 
 export default App;
