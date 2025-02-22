49cf6cf51f5e1f2b148a6e07eb57e7ceb5b72933
Catalin Danis <geaigelsaflu@gmail.com>
2025-02-22 16:32:58 +0200

    resolved ui bugs

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,7 @@ import {useState} from "react";
 
 function App() {
 
-    const [amount, setAmount] = useState(0);
+    const [amount, setAmount] = useState(null);
     const [date, setDate] = useState(null);
     const [type, setType] = useState(null);
     const [firstRadio, setFirstRadio] = useState(null);
@@ -17,33 +17,37 @@ function App() {
                 <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                     <div style={{marginRight: 20}}>
                         <h4 style={{marginBottom: 0}}>Amount</h4>
-                        <input type={"number"} onChange={setAmount}/>
+                        <input type={"number"} onChange={e => setAmount(e.target.value)}/>
                     </div>
                     <div style={{marginRight: 20}}>
                         <h4 style={{marginBottom: 0}}>Date</h4>
-                        <input type={"date"} onChange={setDate}/>
+                        <input type={"date"} onChange={e => setDate(e.target.value)}/>
                     </div>
                     <div style={{marginRight: 20}}>
                         <h4 style={{marginBottom: 0}}>Type</h4>
                         <label style={{marginRight: 10}}>
-                            <input type={"radio"} onChange={() => setType("Enter")}
-                                   checked={type == "Enter" ? true : false}/>
+                            <input type={"radio"} onChange={() => setType("enter")}
+                                   checked={type == "enter" ? true : false}/>
                             Enter
                         </label>
                         <label>
-                            <input type={"radio"} onChange={() => setType("Exit")}
-                                   checked={type == "Exit" ? true : false}/>
+                            <input type={"radio"} onChange={() => setType("exit")}
+                                   checked={type == "exit" ? true : false}/>
                             Exit
                         </label>
                     </div>
                     <div>
-                        <button style={{marginTop: 45}}>
+                        <button style={{marginTop: 45}} onClick={() => {
+                            console.log(amount)
+                            console.log(date)
+                            console.log(type)
+                        }}>
                             Add transaction
                         </button>
                     </div>
                 </div>
             </div>
-            );
-            }
+    );
+}
 
-            export default App;
+export default App;
