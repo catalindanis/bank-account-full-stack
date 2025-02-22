7f64bb148db8f12fb801bec5eaf57570c376bf3b
Catalin Danis <geaigelsaflu@gmail.com>
2025-02-22 16:23:50 +0200

    Added add transaction ui elements

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+    const [amount, setAmount] = useState(0);
+    const [date, setDate] = useState(null);
+    const [type, setType] = useState(null);
+    const [firstRadio, setFirstRadio] = useState(null);
+
+    return (
+        <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+            <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                <img src="./logo.png" style={{height: 25, marginRight: 10}}/>
+                <h1>Bank account</h1>
+            </div>
+                <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
+                    <div style={{marginRight: 20}}>
+                        <h4 style={{marginBottom: 0}}>Amount</h4>
+                        <input type={"number"} onChange={setAmount}/>
+                    </div>
+                    <div style={{marginRight: 20}}>
+                        <h4 style={{marginBottom: 0}}>Date</h4>
+                        <input type={"date"} onChange={setDate}/>
+                    </div>
+                    <div style={{marginRight: 20}}>
+                        <h4 style={{marginBottom: 0}}>Type</h4>
+                        <label style={{marginRight: 10}}>
+                            <input type={"radio"} onChange={() => setType("Enter")}
+                                   checked={type == "Enter" ? true : false}/>
+                            Enter
+                        </label>
+                        <label>
+                            <input type={"radio"} onChange={() => setType("Exit")}
+                                   checked={type == "Exit" ? true : false}/>
+                            Exit
+                        </label>
+                    </div>
+                    <div>
+                        <button style={{marginTop: 45}}>
+                            Add transaction
+                        </button>
+                    </div>
+                </div>
+            </div>
+            );
+            }
+
+            export default App;
