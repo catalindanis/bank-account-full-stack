8ab322b0f6dc1e90ba7f5a9b0c11fbd5e7e0156c
Catalin Danis <geaigelsaflu@gmail.com>
2025-02-22 16:46:30 +0200

    added ui data validation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ function App() {
     const [date, setDate] = useState(null);
     const [type, setType] = useState(null);
     const [firstRadio, setFirstRadio] = useState(null);
+    const [operationMessage, setOperationMessage] = useState(null);
 
     return (
         <div className="App" style={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
@@ -17,7 +18,7 @@ function App() {
                 <div style={{display: 'flex', flexDirection: 'row', alignItems: 'center'}}>
                     <div style={{marginRight: 20}}>
                         <h4 style={{marginBottom: 0}}>Amount</h4>
-                        <input type={"number"} onChange={e => setAmount(e.target.value)}/>
+                        <input type={"number"} onChange={e => setAmount(e.target.value)} placeholder="Enter amount:"/>
                     </div>
                     <div style={{marginRight: 20}}>
                         <h4 style={{marginBottom: 0}}>Date</h4>
@@ -38,12 +39,26 @@ function App() {
                     </div>
                     <div>
                         <button style={{marginTop: 45}} onClick={() => {
-                            
+                            if(amount == null || amount <= 0) {
+                                setOperationMessage("Please enter amount");
+                                return;
+                            }
+                            if(isNaN(new Date(date))) {
+                                setOperationMessage("Please enter a valid date");
+                                return;
+                            }
+                            if(type == null) {
+                                setOperationMessage("Please select type");
+                                return;
+                            }
+
+                            setOperationMessage("Transaction added successfully");
                         }}>
                             Add transaction
                         </button>
                     </div>
                 </div>
+                <p>{operationMessage}</p>
             </div>
     );
 }
